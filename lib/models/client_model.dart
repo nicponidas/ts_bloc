5148d3b6b3217fbfa0291a4f04287aef48685bbb
pawel <pawel.jaron@gmail.com>
2025-03-02 18:53:28 +0100

    początek dodawania supabase

--- lib/models/client_model.dart
+++ lib/models/client_model.dart
@@ -1,5 +1,5 @@
 class ClientModel {
-  final String id;
+  final int id;
   final String name;
   final String shortName;
   final int clientColor;
@@ -22,18 +22,12 @@ class ClientModel {
 
   factory ClientModel.fromMap(Map<String, dynamic> map) {
     return ClientModel(
-      id: map['id'] as String,
+      id: map['id'] as int,
       name: map['name'] as String,
       shortName: map['shortName'] as String,
       clientColor: map['clientColor'] as int,
     );
   }
 
-  ClientModel copyWith({String? id, String? name}) {
-    return ClientModel(
-        id: id ?? this.id,
-        name: name ?? this.name,
-        shortName: shortName,
-        clientColor: clientColor);
-  }
+
 }
